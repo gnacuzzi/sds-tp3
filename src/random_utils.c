c8dcf8274b7090b21910c2b41746c7283fa7f2d4
lautipaletta <lapaletta@itba.edu.ar>
2026-04-10 11:39:58 -0300

    fix: add random seed

--- src/random_utils.c
+++ src/random_utils.c
@@ -2,23 +2,16 @@
 #include "config.h"
 
 #include <stdlib.h>
+#include <time.h>
 
-
-void random_seed(unsigned int seed) {
-    srand(seed);
-}
-
+void random_seed(unsigned int seed) { srand(time(NULL)); }
 
 double random_uniform(void) {
-    return (double) rand() / ((double) RAND_MAX + 1.0);
+  return (double)rand() / ((double)RAND_MAX + 1.0);
 }
-
 
 double random_uniform_range(double min, double max) {
-    return min + (max - min) * random_uniform();
+  return min + (max - min) * random_uniform();
 }
 
-
-double random_angle(void) {
-    return random_uniform_range(0.0, 2.0 * M_PI);
-}
+double random_angle(void) { return random_uniform_range(0.0, 2.0 * M_PI); }
