cc2df2cf84d78dce18ee4891cd5397368d399fb7
gnacuzzi <gnacuzzi@itba.edu.ar>
2026-04-18 09:00:34 -0300

    feat: implement logarithmic scaling in performance plots and create separate log plot script

--- python/plot_performance_log.py
+++ python/plot_performance_log.py
@@ -1,6 +1,10 @@
 import csv
 import matplotlib.pyplot as plt
 
+# Escalas logaritmicas (ejemplo tipico: log-log)
+USE_LOG_X = True
+USE_LOG_Y = True
+
 
 ns = []
 times = []
@@ -24,6 +28,16 @@ plt.plot(
     linestyle="-"
 )
 
+if USE_LOG_X:
+    if any(n <= 0 for n in ns):
+        raise ValueError("N must be > 0 to use logarithmic X scale")
+    plt.xscale("log")
+
+if USE_LOG_Y:
+    if any(t <= 0 for t in times):
+        raise ValueError("Execution times must be > 0 to use logarithmic Y scale")
+    plt.yscale("log")
+
 plt.xlabel("Number of Particles (N)", fontsize=14)
 plt.ylabel("Execution Time (s)", fontsize=14)
 plt.xticks(fontsize=14)
@@ -32,6 +46,6 @@ plt.yticks(fontsize=14)
 #plt.ylim(0, max(times) * 1.5)
 plt.tight_layout()
 
-plt.savefig("images/performance_plot.png", dpi=300)
+plt.savefig("images/performance_plot_log.png", dpi=300)
 
 plt.show()
